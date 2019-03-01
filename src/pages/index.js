1a3d90299d06f616927c86a5d5a83329cc625514
Maxwell Kirwin <maxwell_kirwin@yahoo.co.uk>
2019-03-01 22:11:53 +0000

    adding the first files

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi I'm Max</h1>
+    <p>Welcome</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
